06ac1ff5b682f6e7daf79cd6e2effd0c3d618521
NikWindup <nikita.uschakow@icloud.com>
2025-02-16 01:06:23 +0100

    Normalized Model

--- api/bookry/models.py
+++ api/bookry/models.py
@@ -1,31 +1,38 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+
 class Book(models.Model):
-    
-    GENRE_CHOICES = [
-        ('FIC', 'Fiction'),
-        ('NON', 'Non-Fiction'),
-        ('SCI', 'Science'),
-        ('HIS', 'History'),
-        ('BIO', 'Biography'),
-        ('FAN', 'Fantasy'),
-        ('THR', 'Thriller'),
-        ('ROM', 'Romance'),
-        ('HOR', 'Horror'),
-        ('OTH', 'Other'),
-    ]
-    
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    genre = models.CharField(max_length=3, choices=GENRE_CHOICES, default='OTH')
-    rating = models.PositiveSmallIntegerField()
-    language = models.CharField(max_length=50)
-    description = models.TextField()
-    cover_image = models.ImageField()
-    started_reading = models.DateTimeField(auto_now_add=True)
-    finished_reading = models.DateTimeField(blank=True, null=True)
-    
-    def __str__(self):
-        return f"{self.title} by {self.author}"
+    title = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True, null=True)
+    published_date = models.DateField(blank=True, null=True)
+    isbn = models.CharField(max_length=13, unique=True, blank=True, null=True)
+    pages = models.PositiveIntegerField(blank=True, null=True)
+    language = models.CharField(max_length=50, default='English')
+    cover_image = models.URLField(blank=True, null=True)
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+class BookAuthor(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+class BookGenre(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+class Rating(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    rating = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ],
+        default=1
+    )
