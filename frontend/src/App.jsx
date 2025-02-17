5dffdcca9f503c7d8d5f99ebd3087e662e3069d0
NikWindup <nikita.uschakow@icloud.com>
2025-02-18 00:33:31 +0100

    connected frontend to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/books/')
+      .then(response => {
+        setBooks(response.data)
+      })
+      .catch(error => {
+        console.error('There was an error fetching the books!', error)
+      })
+  }, [])
 
   return (
     <div>
-      Hello
+      <h1>Books</h1>
+      <ul>
+        {books.map(book => (
+          <li key={book.id}>{book.title}</li>
+        ))}
+      </ul>
     </div>
   )
 }
 
-export default App
+export default App;
