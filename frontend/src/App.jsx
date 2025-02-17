8c27ab5d186094c5a8b4c7ebfd78c29b7b48864d
NikWindup <nikita.uschakow@icloud.com>
2025-02-18 00:48:25 +0100

    readme updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,28 +3,10 @@ import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [books, setBooks] = useState([])
+  axios.get('http://loacalhost:8000/books/').then(response => {console.log(response.data)})
 
-  useEffect(() => {
-    axios.get('http://localhost:8000/books/')
-      .then(response => {
-        setBooks(response.data)
-      })
-      .catch(error => {
-        console.error('There was an error fetching the books!', error)
-      })
-  }, [])
-
-  return (
-    <div>
-      <h1>Books</h1>
-      <ul>
-        {books.map(book => (
-          <li key={book.id}>{book.title}</li>
-        ))}
-      </ul>
-    </div>
-  )
+  return ("hello")
+    
 }
 
 export default App;
