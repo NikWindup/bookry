1fbbaf7654d37482960fc37c285ad38209ce94a1
NikWindup <nikita.uschakow@icloud.com>
2025-02-19 23:30:55 +0100

    practising (following 30-days-of-react) #3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,26 +2,33 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
 
-const author = {
-    firstName: "Kristina",
-    lastName: "Ziegler"
-}
 
-const header = (
-  <header>
-    <div className='header-wrapper'>
-        <h2>Books</h2>
-    </div>
-  </header>
+
+const button = (
+    <button>Subscribe</button>
 )
 
+const inputs = (
+    <>
+        <div className='input-wrapper'>
+            <input type="firstName" placeholder='First Name'/>
+            <input type="lastName" placeholder='Last Name'/>
+            <input type="email" placeholder="E-Mail"/>
+        </div>
+    </>
+)
 
 function App() {
-
-  return (
-    [header]
-  )
-    
+    return (
+        <>
+            <div className='subscribe-wrapper'>
+                <h1>SUBSCRIBE</h1>
+                <p>Sign up with your email address to receive news and updates</p>
+                {inputs}
+                {button}
+            </div>
+        </>
+    )
 }
 
 export default App;
