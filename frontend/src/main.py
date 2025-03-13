d01e323a08252e5fb77a55dddd51959583665a8f
NikWindup <nikita.uschakow@icloud.com>
2025-03-14 00:01:31 +0100

    Learning Flet

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -1,47 +1,51 @@
 import flet as ft
 from flet.core.control_event import ControlEvent
+from flet.core.page import RouteChangeEvent
 
 
 def main(page: ft.Page):
-    def check_credentials(e: ControlEvent):
+
+    def main_menu(e: ControlEvent):
         if username.value and password.value:
             print(username.value, password.value)
             print(page.route)
-
-    def switch_page(e: ControlEvent):
-        if page.route == "/":
+            page.window.resizable = True
             page.clean()
             page.add(
                 ft.Row(
                     controls=[
-                        ft.IconButton(icon=ft.icons.ARROW_BACK_IOS),
-                        ft.IconButton(icon=ft.icons.SETTINGS)
+                        ft.IconButton(icon=ft.Icons.ARROW_BACK_IOS),
+                        ft.IconButton(icon=ft.Icons.SETTINGS)
                     ],
                     alignment=ft.MainAxisAlignment.SPACE_EVENLY
                 ),
                 ft.Column(
-                    controls=[]
+                    controls=[
+                        ft.ElevatedButton(text="Join"),
+                        ft.ElevatedButton(text="Create Game"),
+                        ft.ElevatedButton(text="Create Tournament")
+                    ],
+                    alignment=ft.MainAxisAlignment.SPACE_EVENLY
                 )
-
             )
-
+            page.update()
 
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.window.width = 200
-    page.window.height = 250
+    page.window.width = 400
+    page.window.height = 450
     page.window.resizable = False
-    page.route = "/"
 
 
     #  Login Page
     username = ft.TextField(width=250, multiline=False)
     password = ft.TextField(width=250, password=True, multiline=False)
-    sign_up_button = ft.Button(text="Sign Up", on_click=check_credentials)
-    login_button = ft.ElevatedButton(text="Log In", on_click=check_credentials)
+    sign_up_button = ft.Button(text="Sign Up", width=100)
+    login_button = ft.ElevatedButton(text="Sign In", on_click=main_menu, width=100)
 
     page.add(
         ft.Column(
             controls=[
+                ft.Text(value="Sign In", text_align=ft.TextAlign.CENTER, size=30),
                 username,
                 password,
                 ft.Row(
@@ -52,7 +56,8 @@ def main(page: ft.Page):
                     alignment=ft.MainAxisAlignment.CENTER
                 )
             ],
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            alignment=ft.MainAxisAlignment.SPACE_EVENLY
         )
     )
 
