77e95ad4cb61a9f41f8161c194f624b3ea6b0081
NikWindup <nikita.uschakow@icloud.com>
2025-03-13 11:04:35 +0100

    Learning Flet

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -1,10 +1,30 @@
 import flet as ft
+from flet.core.control_event import ControlEvent
 
 
 def main(page: ft.Page):
-    def check_credentials(e: ft.ControlEvent):
+    def check_credentials(e: ControlEvent):
         if username.value and password.value:
             print(username.value, password.value)
+            print(page.route)
+
+    def switch_page(e: ControlEvent):
+        if page.route == "/":
+            page.clean()
+            page.add(
+                ft.Row(
+                    controls=[
+                        ft.IconButton(icon=ft.icons.ARROW_BACK_IOS),
+                        ft.IconButton(icon=ft.icons.SETTINGS)
+                    ],
+                    alignment=ft.MainAxisAlignment.SPACE_EVENLY
+                ),
+                ft.Column(
+                    controls=[]
+                )
+
+            )
+
 
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.window.width = 200
@@ -16,8 +36,8 @@ def main(page: ft.Page):
     #  Login Page
     username = ft.TextField(width=250, multiline=False)
     password = ft.TextField(width=250, password=True, multiline=False)
-    sign_up_button = ft.ElevatedButton(text="Sign Up")
-    login_button = ft.ElevatedButton(text="Log In")
+    sign_up_button = ft.Button(text="Sign Up", on_click=check_credentials)
+    login_button = ft.ElevatedButton(text="Log In", on_click=check_credentials)
 
     page.add(
         ft.Column(
