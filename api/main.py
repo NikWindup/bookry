b1cc5a8c3dadb73ad482d4764410dfb3de6853e9
NikWindup <127618005+NikWindup@users.noreply.github.com>
2025-04-24 13:10:43 +0000

    Codespace

--- api/main.py
+++ api/main.py
@@ -3,15 +3,39 @@ Tutorial: https://fastapi.tiangolo.com/tutorial
 """
 
 from fastapi import FastAPI
+from fastapi.C
 from SetupDB import SetupDB
 from dao.BookDao import BookDao
 from dao.GenreDao import GenreDao
 from dao.BookGenreDao import BookGenreDao
 from dao.AuthorDao import AuthorDao
+from dao.UserDao import UserDao
 from schemas.Book import Book
+from schemas.User import User
 
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/users")
+async def post_user(user: User):
+    UserDao.insert(user.email, user.username)
 
 @app.get("/books/{book_id}")
 async def get_book(book_id):
@@ -20,4 +44,5 @@ async def get_book(book_id):
 
 @app.post("/books/")
 async def post_book(book: Book):
+    BookDao.insert(book)
     return {"message": "Succesfully added book","book": dict(book)}
--- frontend/src/test.py
+++ frontend/src/test.py
@@ -2,12 +2,15 @@ import flet as ft
 import requests
 
 
-url = "http://127.0.0.1:8000/books/1"
+url = "https://silver-winner-5gvpxw4gr6wfpxqp-8000.app.github.dev/books/1"
 
 def main(page: ft.Page):
     
     data = requests.get(url=url)
     
-    print(data.content)
+    data = dict(data.content)
+
+    for k, v in data.items():
+        ft.Text(value=v)
     
 ft.app(target=main)
